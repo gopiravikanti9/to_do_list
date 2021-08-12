20e15f375eb208c5ae375d2a101c17a0b3fda584
GOPICHAND Ravikanti <ravikanti.gopichand@soprasteria.com>
2021-08-12 09:59:18 +0530

    To_Do_List app

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ToDo from "./ToDo";
+function App() {
+  return (
+    <div className="container">
+      <header className="App-header">
+        <h1>To Do List Application</h1>
+
+        <ToDo />
+      </header>
+    </div>
+  );
+}
+
+export default App;
